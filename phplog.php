c64e2849cdd24497149d9c119360c95bce742171
Hunter Graves <server.hunter@gmail.com>
2017-01-13 04:40:45 -0800

    Add files via upload

---
+++ phplog.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<p>
+
+
+<?php $firstname = $_GET["firstname"]; ?>
+<?php $lastname = $_GET["lastname"]; ?>
+<?php $email = $_GET["email"]; ?>
+Thanks<?php echo  $_GET["firstname"] $_GET["lastname"];?>
+Your email address is:<?php echo  $_GET["email"];?>
+
+
+</p>
+</body>
+</html>
+
+
